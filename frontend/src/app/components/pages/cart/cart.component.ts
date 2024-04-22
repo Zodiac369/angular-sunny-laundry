7e5866121d4e52cf9d341c59e08dfff58602a333
3MRE <emre.ogk@hotmail.fr>
2024-04-23 00:23:14 +0200

    update

--- frontend/src/app/components/pages/cart/cart.component.ts
+++ frontend/src/app/components/pages/cart/cart.component.ts
@@ -9,24 +9,27 @@ import { CartItem } from 'src/app/shared/models/CartItem';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  cart!: Cart;
+  cart!: Cart; // Déclaration d'une variable pour contenir le panier
 
   constructor(private cartService: CartService) {
+    // Abonnement à l'observable du service de panier pour mettre à jour le panier
     this.cartService.getCartObservable().subscribe((cart) => {
-      this.cart = cart;
+      this.cart = cart; // Mettre à jour le panier lorsque des changements sont détectés
     })
-   }
+  }
 
-   
-   removeFromCart(cartItem: CartItem) {
-     this.cartService.removeFromCart(cartItem.product.id);
-    }
+  // Méthode pour retirer un article du panier
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.product.id);
+  }
+  
+  // Méthode pour changer la quantité d'un article dans le panier
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.product.id, quantity);
+  }
+  
+  ngOnInit(): void {
     
-    changeQuantity(cartItem: CartItem, quantityInString: string) {
-      const quantity = parseInt(quantityInString);
-      this.cartService.changeQuantity(cartItem.product.id, quantity);
-    }
-    
-    ngOnInit(): void {
-    }
+  }
 }
